a41ac0da201c7dd920e9fd319b2ac856f0a38e1a
Glitch (timestamp-microservice-tsms) <none>
2018-01-18 04:25:22 +0000

    👱🔖 Checkpoint routes/index.js:576250/374

--- routes/index.js
+++ routes/index.js
@@ -16,14 +16,13 @@ router.get('/:time', (req, res) => {
   const enteredTime = req.params.time;
 
   function processTime(date) {
-    const nulled = {
-    unix:null,
-      natural:null
-    }
     //define whether input is natural language or numbers (see if it has anything other than numbers or is just a number)
     if (isNaN(date.getTime())) { // d.valueOf() could also work
       // date is not valid
-      response = nulled;
+      response = {
+        unix: null,
+        natural: null
+      }
     } else { // date is valid
       const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
       const unix = date.getTime() / 1000;
@@ -31,26 +30,16 @@ router.get('/:time', (req, res) => {
       const year = date.getFullYear();
       const day = `0${date.getDate()}`.slice(-2);
       const natural = `${month} ${day}, ${year}`;
-      const valid = {
-      unix,
+      response = {
+        unix,
         natural
-      }
-      //checking if a year is a leap year - divisible by 4
-      if (month == "February" && day == 29 && year % 4 == 0) {
-        //leap years occur every 4 years, except for years divisible by 100 and not divisible by 400
-        if (year % 100 == 0 && year % 400 != 0) {
-          console.log(day);
-          response = nulled;
-        } else {
-          response = valid;
-        }
-      } else {
-      response = valid;
-      }
+      };
     }
   }
   //need to convert number strings to numbers to make them work otherwise they return a weird date
-  if (!isNaN(+enteredTime)) {
+  if (enteredTime.toLowerCase() == "now") {
+    processTime(new Date(Date.now()));
+  } else if (!isNaN(+enteredTime)) {
     processTime(new Date(+enteredTime * 1000));
   } else {
     processTime(new Date(enteredTime));
