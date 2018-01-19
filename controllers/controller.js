2279686ce4e1b720b896334c34f734e0f3ec055b
Glitch (glossy-list) <none>
2018-01-19 04:11:28 +0000

    🐑🏏 Checkpoint controllers/controller.js:576250/128 package.json:576250/29

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,11 @@
+const userAgentParser = require("buster-user-agent-parser");
+
 exports.whoAmI = (req, res) => {
   const data = req.headers;
   const ipaddress = data['x-forwarded-for'].split(",")[0];
-  const language =[...data['user-agent']]
+  const userAgent = data['user-agent'];
 console.log(req.headers['x-forwarded-for']);
   console.log(req.headers['user-agent']);
   console.log(req.headers['accept-language']);
-res.send(ipaddress);
+res.send(ip);
 }
